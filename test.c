ae288702085253d0e7954ae7273900db64ee4fcd
onestonec <winstoncheung2014@gmail.com>
2022-05-21 08:24:28 -0700

    Start at strncat

--- test.c
+++ test.c
@@ -8,11 +8,11 @@ int main(){
    char s[6] = {'s', 't', 'r', 'i', 'n', 'g'};
   
    // Appends 5 character from src to dest
-   ft_strlcat(dest, src, 5);
+   //ft_strlcat(dest, src, 5);
      
    // Prints the string
-   printf("Source string : %s\n", src);
-   printf("Destination string : %s", dest);
+   printf("ft_isalnum:  %d\n", ft_isalnum('1'));
+   //printf("Destination string : %s", dest);
    
    return(0);
 }
